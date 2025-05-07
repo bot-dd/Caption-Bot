54535aecbf6faee05d7068f956fdd2d98b568726
bot-dd <botdd445@gmail.com>
2025-05-07 14:13:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7545544173:AAFLyGXW3shlfcxDfd5CZ9X4JTBjlXkAGqs")
+      API_ID = int(os.environ.get("APP_ID", 26649585))
+      API_HASH = os.environ.get("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
       CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "AvishkarPatil")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "RahatMx")
